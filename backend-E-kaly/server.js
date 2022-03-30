49aab99cf0a117307d4af9e8d74bce4109bca3e6
kiady11 <kiady.rakotoharinjatovo@yahoo.com>
2022-03-30 20:55:22 +0300

    simple connection to local database

--- backend-E-kaly/server.js
+++ backend-E-kaly/server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
+//connect to server
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
 
+// connect to mongoDB
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'testMongoDB';
+let db
+
+MongoClient.connect(url).then(client => {
+    db = client.db(dbName);
+    console.log(`Connected MongoDB: ${url}`);
+    console.log(`Database: ${dbName}`);
+}).catch(console.error);
+
+//body parser . get parameter's send by client
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// routage
 app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-})
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+  });
